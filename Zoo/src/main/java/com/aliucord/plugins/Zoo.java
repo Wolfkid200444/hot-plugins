df884746b4afccd8988db8c85718daabaa916227
Wolfie <Wolfkid200444@gmail.com>
2021-08-14 00:28:36 -0400

    added license

--- Zoo/src/main/java/com/aliucord/plugins/Zoo.java
+++ Zoo/src/main/java/com/aliucord/plugins/Zoo.java
@@ -1,3 +1,13 @@
+/*
+ * Wolf's Aliucord Plugins
+ * Copyright (C) 2021 Wolfkid200444
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.aliucord.plugins;
 
 import android.content.Context;
